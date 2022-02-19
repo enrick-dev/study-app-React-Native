264700d12d58f3dd63a973626c40bcf15bf0e94c
Enrick Santos de Souza <enricksantos03@gmail.com>
2022-02-18 23:10:43 -0300

    exercice click times

--- App.js
+++ App.js
@@ -2,16 +2,19 @@ import React, {useState} from 'react';
 import {StyleSheet, View, Text, Button, Linking} from 'react-native';
 
 const App = () => {
-  const [name, setName] = useState('Enrick');
+  const [click, setClick] = useState(0)
+  const [count, setCount] = useState(0)
 
   const onClickHandler = () => {
-    setName('Programando com Enrick');
+    setClick(click + 3)
+    setCount(count + 1)
   };
 
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>{name}</Text>
+      <Text style={styles.text}>{click}</Text>
       <Button title="Update" onPress={onClickHandler} />
+      <Text style={styles.text}>You clicked {count} times</Text>
     </View>
   );
 };
